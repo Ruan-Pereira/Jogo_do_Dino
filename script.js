45d8511a78af240412e468fa825d37892c2167f4
Ruan-Pereira <ruanborbapereira@gmail.com>
2020-11-18 21:23:28 -0300

    criando a função createCactus

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
+
 
 function hadnleKeyUp(event) {
     if (event.keyCode === 32) {
-      jump();
+     if(!isJumping)(
+            jump()
+        ) 
     }
 }
 
 function jump() {
     let position = 0;
+
+    isJumping = true;
 
     let upInterval = setInterval(() => {
        if(position >= 150){
@@ -16,6 +23,7 @@ function jump() {
         let downInterval = setInterval(() => {
         if (position <= 0) {
             clearInterval(downInterval);
+            isJumping = false
         } else {
             position -= 20;
             dino.style.bottom = position + "px"
@@ -27,5 +35,27 @@ function jump() {
     }
     }, 20);
 }
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randowTime = Math.random() * 6000;
 
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px' ;
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if(cactusPosition < -60 ) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randowTime);
+}
+createCactus();
 document.addEventListener('keyup', hadnleKeyUp);
